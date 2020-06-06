54449f4b375a145dfda382a7acf78ac66e84bd29
Nachichuri <nachichuri@gmail.com>
2020-06-06 16:41:56 -0300

    Agregado Scaper ICBC

--- src/main/java/me/divisa/bankscraper/Main.java
+++ src/main/java/me/divisa/bankscraper/Main.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class Main {
@@ -16,12 +17,19 @@ public class Main {
 
         ScraperBbva bbva = new ScraperBbva(driver, espera);
         ScraperCiudad ciudad = new ScraperCiudad(driver, espera);
+        ScraperIcbc icbc = new ScraperIcbc(driver, espera);
 
         System.out.println("Cotizaciones BBVA:");
         System.out.println(Arrays.toString(bbva.getList()));
 
         System.out.println("Cotizaciones Ciudad:");
         System.out.println(Arrays.toString(ciudad.getList()));
+
+        System.out.println("Cotizaciones Galicia:");
+        System.out.println("Falta hacer :(");
+
+        System.out.println("Cotizaciones ICBC:");
+        System.out.println(Arrays.toString(icbc.getList()));
 
         //Cerramos el navegador
         driver.close();
---
+++ src/main/java/me/divisa/bankscraper/ScraperIcbc.java
@@ -0,0 +1,55 @@
+package me.divisa.bankscraper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScraperIcbc implements Scraper {
+
+    private final String TARGET_URL;
+    private final WebDriver NAVEGADOR;
+    private final WebDriverWait ESPERA;
+
+    // Constructor
+    public ScraperIcbc(WebDriver driver, WebDriverWait wait) {
+        this.TARGET_URL = "https://www.icbc.com.ar/personas";
+        this.NAVEGADOR = driver;
+        this.ESPERA = wait;
+    }
+
+    // Interface overriding
+    @Override
+    public double[] getList() {
+        return scrape();
+    }
+
+    public double[] scrape() {
+        ArrayList<Double> cotizaciones = new ArrayList<Double>();
+
+        // Let's go
+        NAVEGADOR.navigate().to(TARGET_URL);
+
+        // Esperamos que renderice la tabla y la guardamos en una lista de objetos scrapeados
+        ESPERA.until(ExpectedConditions.visibilityOfElementLocated(By.className("moneda-datos")));
+        List<WebElement> tabla = NAVEGADOR.findElements(By.className("moneda--valor"));
+
+        // Como devuelven strings con mas caracteres, lo que hacemos es iterar sobre cada uno, crear substrings
+        // y después parsearlos y agregarlos al ArrayList
+        for (WebElement item : tabla) {
+            cotizaciones.add(0,
+                    Double.parseDouble(
+                    item.getText()
+                        .substring(item.getText().indexOf("$")+1, item.getText().indexOf("$")+6)
+                        .replace(",", ".")
+                    ));
+        }
+
+        // Creamos un stream con el ArrayList y devolvemos los valores en un double[]
+        return cotizaciones.stream().mapToDouble(d -> d).toArray();
+    }
+}
